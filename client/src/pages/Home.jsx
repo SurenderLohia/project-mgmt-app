525108ad2f5cc5cb7f20b6f8538895bacf4d9988
SurenderLohia <surender.lohia.e@gmail.com>
2022-10-31 11:49:42 +0530

    Add client side crud functionality for project

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import AddClientModal from '../components/AddClientModal';
+import AddProjectModal from '../components/AddProjectModal';
 import Client from '../components/Client';
 import Projects from '../components/Projects';
 
 export default function Home() {
   return (
     <>
-      <AddClientModal />
+      <div className="d-flex gap-3 mb-4">
+        <AddClientModal />
+        <AddProjectModal />
+      </div>
       <Projects />
       <Client />
     </>
--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 export default function NotFound() {
   return (
     <div className="d-flex flex-column justify-content-center align-items-center mt-5">
-      <FaExclamationTriangle className="text-danger" size="5em" />
+      <FaExclamationTriangle className="text-danger mb-3" size="5em" />
       <h1>404</h1>
       <p className="lead">Sorry, this page does not exist</p>
       <Link to="/" className="btn btn-primary">
